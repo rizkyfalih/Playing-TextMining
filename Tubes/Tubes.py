9de3c7cc5595a7c36e2a0f68f6ae5e1a44275667
rizkyfalih <rizkyfalihpridyandhika@ymail.com>
2018-04-08 09:46:34 +0700

    Done!!!

--- Tubes/Tubes.py
+++ Tubes/Tubes.py
@@ -19,7 +19,7 @@ from xml.dom import minidom
 # Importing classification libraries
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 from sklearn.naive_bayes import GaussianNB
 
 def label_to_binary(data):
@@ -30,8 +30,8 @@ def label_to_binary(data):
             data.loc[i, 'Binary'] = 0
     return data
 
-def get_headline_training(filename):
-    xmldoc = minidom.parse('Dataset/Training-Set/'+ filename + '.xml')
+def get_headline_set(filename):
+    xmldoc = minidom.parse('Dataset/Data-Set/'+ filename + '.xml')
     headlineList = xmldoc.getElementsByTagName('headline')
     headline = ""
     for i in range(0, len(headlineList)):
@@ -39,8 +39,8 @@ def get_headline_training(filename):
     
     return headline
  
-def get_article_training(filename):
-    xmldoc = minidom.parse('Dataset/Training-Set/'+ filename + '.xml')
+def get_article_set(filename):
+    xmldoc = minidom.parse('Dataset/Data-Set/'+ filename + '.xml')
     paragraphList = xmldoc.getElementsByTagName('p')
     paragraph = ""
     for i in range(0, len(paragraphList)):
@@ -48,39 +48,12 @@ def get_article_training(filename):
     
     return paragraph
 
-def get_headline_testing(filename):
-    xmldoc = minidom.parse('Dataset/Testing-Set/'+ filename + '.xml')
-    headlineList = xmldoc.getElementsByTagName('headline')
-    headline = ""
-    for i in range(0, len(headlineList)):
-        headline += headlineList[i].firstChild.nodeValue
-    
-    return headline
- 
-def get_article_testing(filename):
-    xmldoc = minidom.parse('Dataset/Testing-Set/'+ filename + '.xml')
-    paragraphList = xmldoc.getElementsByTagName('p')
-    paragraph = ""
-    for i in range(0, len(paragraphList)):
-        paragraph += paragraphList[i].firstChild.nodeValue
-    
-    return paragraph
-
-def join_all_text_training(filename):
+def join_all_text_set(filename):
     corpus = ""
     # Get headline
-    corpus += get_headline_training(filename)
+    corpus += get_headline_set(filename)
     # Get article and join with headline
-    corpus = " " + get_article_training(filename)
-    
-    return corpus
-
-def join_all_text_testing(filename):
-    corpus = ""
-    # Get headline
-    corpus += get_headline_testing(filename)
-    # Get article and join with headline
-    corpus = " " + get_article_testing(filename)
+    corpus = " " + get_article_set(filename)
     
     return corpus
 
@@ -96,50 +69,41 @@ def cleaned_text(data):
         newCorpus.append(corpus)
     return newCorpus
 
-def get_data_training(data):
+def get_data_set(data):
     for i in data.index:
-        data.loc[i, 'Corpus'] = join_all_text_training(str(data.loc[i, 'Filename']))
+        data.loc[i, 'Corpus'] = join_all_text_set(str(data.loc[i, 'Filename']))
         
-    return data       
-
-def get_data_testing(data):
-    for i in data.index:
-        data.loc[i, 'Corpus'] = join_all_text_testing(str(data.loc[i, 'Filename']))
-        
-    return data       
+    return data         
 
 def bag_of_words(corpus):
-    cv = CountVectorizer(max_features = 1237)
+    cv = CountVectorizer()
     X = cv.fit_transform(corpus).toarray()
     
     return X
 
-# Initialize Data Training
-trainingSet = get_data_training(label_to_binary(pd.read_csv('Dataset/Label/Label-Training.txt', delimiter = ' ')))
-corpusTraining = cleaned_text(trainingSet)
-X_train = bag_of_words(corpusTraining)
-y_train = trainingSet.iloc[:, 2].values
+# Initialize Data Set
+dataSet = get_data_set(label_to_binary(pd.read_csv('Dataset/Label/Label-Set.txt', delimiter = ' ')))
+corpusSet = cleaned_text(dataSet)
+X = bag_of_words(corpusSet)
 
-# Initialize Data Testing
-testingSet = get_data_testing(label_to_binary(pd.read_csv('Dataset/Label/Label-Testing.txt', delimiter = ' ')))
-corpusTesting = cleaned_text(testingSet)
-X_test = bag_of_words(corpusTesting)
-y_test = testingSet.iloc[:, 2].values
+X_train = X[:577]
+y_train = dataSet.iloc[:577, 2].values
+
+X_test = X[577:]
+y_test = dataSet.iloc[577:, 2].values
 
 # Classifier Multinomial Naive Bayes
-classifier = MultinomialNB()
-classifier.fit(X_train, y_train)
-y_pred = classifier.predict(X_test)
+classifier1 = MultinomialNB()
+classifier1.fit(X_train, y_train)
+y_pred1 = classifier1.predict(X_test)
 
-cm1 = confusion_matrix(y_test, y_pred)
-accuracy1 = ((cm1[0][0]+cm1[1][1])/len(X_test))*100
+accuracy1 = accuracy_score(y_test, y_pred1) * 100
 print("Accuracy Multinomial = " + str(accuracy1))
 
 # Classifier Naive Bayes
-classifier = GaussianNB()
-classifier.fit(X_train, y_train)
-y_pred = classifier.predict(X_test)
+classifier2 = GaussianNB()
+classifier2.fit(X_train, y_train)
+y_pred2 = classifier2.predict(X_test)
 
-cm2 = confusion_matrix(y_test, y_pred)
-accuracy2 = ((cm2[0][0]+cm2[1][1])/len(X_test))*100
+accuracy2 = accuracy_score(y_test, y_pred2) * 100
 print("Accuracy Naive Bayes = " + str(accuracy2))
